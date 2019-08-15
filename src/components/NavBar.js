1532a2b059d8a22c01b829c0621eabfeea4d20bc
brianryu <s.brianryu@gmail.com>
2019-08-15 14:16:54 -0400

    NavBar show - Currently working on Home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,25 @@ export default function Navbar(props) {
 
     return (
       <div>
-          hello
-          {/* login form */}
-          {/* signup form */}
+          <nav className="navbar">
+            <div>
+            <br />
+            <a className="mainlogo navlink" name="STEEZ." href="/">STEEZ.</a>
+                <div>
+                    <button type="button" className="navlink" name="Register">REGISTER</button>
+
+                    {/* for now, I am just keeping the Register Button and LogIn button option. 
+                    Figure out the user Auth later - with the login terinary option */}
+
+                    
+                    {/* {this.props.currentUser 
+                    ? (<button className="navlink" name="Logout">LOGOUT</button>) :  */}
+                    
+                    <button className="navlink" name="Login">LOGIN</button>
+                
+                </div>
+            </div>
+        </nav>
       </div>
-      );
+    );
 }
