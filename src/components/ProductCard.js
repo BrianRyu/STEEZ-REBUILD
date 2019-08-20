fc0088d8cd4fe21863ac0b08292f80df49d0da71
brianryu <s.brianryu@gmail.com>
2019-08-19 21:58:19 -0400

    product card currently in the work

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default function ProductCard(props) {
     return(
-        <div>
+        <div className="grid-item">
             <p className="product-font">{props.product.name}</p>
+            <br/>
             <Link to={`/product/${props.product.id}`}>
                 <img className="product-image" src={props.product.img_url} alt={props.product.name}/>
             </Link>
+            <br/>
         </div>
     )
 }
