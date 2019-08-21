ea5c6e2ef42baa41cdb4da33fe62dfb495ff66de
brianryu <s.brianryu@gmail.com>
2019-08-21 12:36:18 -0400

    still working on product show

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 export default function ProductCard(props) {
+
+    function handleClick(e){
+        console.log(e.target.id)
+        
+    }
+
+    
     return(
         <div className="grid-item">
             <p className="product-font">{props.product.name}</p>
             <br/>
-            <Link to={`/product/${props.product.id}`}>
-                <img className="product-image" src={props.product.img_url} alt={props.product.name}/>
-            </Link>
+            <img onClick={handleClick} id={props.product.id} className="product-image" src={props.product.img_url} alt={props.product.name}/>
             <br/>
         </div>
     )
